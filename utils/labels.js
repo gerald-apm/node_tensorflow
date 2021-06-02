0b0e829691bd724d5585a4f37b3e13acfb760f95
gerald-apm <geraldiaditya1111@gmail.com>
2021-06-02 20:12:04 +0700

    fixed labeling on ML models

--- utils/labels.js
+++ utils/labels.js
@@ -1,27 +1,27 @@
 const labelcorn = [
-    'healthy',
+    'cercospora leaf spot gray leaf spot',
     'common rust',
     'northern leaf blight',
-    'cercospora leaf spot',
+    'healthy',
 ];
 
 const labelpotato = [  
-    'healthy',
-    'late blight',
     'early blight',
+    'late blight',
+    'healthy',  
 ];
 
 const labeltomato = [
-    'leaf mold',
-    'target spot',
-    'yellow leaf curlvirus',
-    'early blight',
-    'mosaic virus',
-    'septoria leaf spot',
-    'spider mites two-spotted spidermite',
-    'late blight',
-    'healthy',
     'bacterial spot',
+    'early blight',
+    'late blight',
+    'leaf mold',
+    'septoria leaf spot',
+    'spider mites or two-spotted spider mite',
+    'target spot',
+    'tomato yellow leaf curl virus',
+    'tomato mosaic virus',
+    'healthy',
 ];
 
 module.exports = {labelcorn, labelpotato, labeltomato};
