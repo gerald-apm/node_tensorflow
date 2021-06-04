bbd19e19155fd430a7a421754802621f61ccb691
Geraldhi A. P. Mahayadnya <geraldiaditya1111@gmail.com>
2021-06-04 19:57:01 +0700

    testing reverse proxy

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const express = require('express');
 const logger = require('morgan');
 const fs = require('fs');
@@ -5,7 +6,7 @@ const http = require('http');
 const https = require('https');
 const app = express();
 const cors = require('cors');
-const porthttp = process.env.PORT_HTTP ||  5000;
+const porthttp = process.env.PORT_HTTP || 5000;
 const porthttps = process.env.PORT_HTTPS || 5050;
 const hostname = require('./utils/localhost');
 const indexRouter = require('./routes/index');
@@ -15,9 +16,9 @@ const potatoRouter = require('./routes/potatopred');
 const uploadRouter = require('./routes/upload');
 // const privateKey  = fs.readFileSync('credentials/selfsigned.key', 'utf8');
 // const certificate = fs.readFileSync('credentials/selfsigned.crt', 'utf8');
-const privateKey = fs.readFileSync('credentials/api.easeplantz.ml/privkey1.pem', 'utf8');
-const certificate = fs.readFileSync('credentials/api.easeplantz.ml/fullchain1.pem', 'utf8');
-const credentials = {key: privateKey, cert: certificate};
+// const privateKey = fs.readFileSync('credentials/api.easeplantz.ml/privkey1.pem', 'utf8');
+// const certificate = fs.readFileSync('credentials/api.easeplantz.ml/fullchain1.pem', 'utf8');
+// const credentials = {key: privateKey, cert: certificate};
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,12 +35,12 @@ app.use('/upload', express.static('html'));
 app.use(cors());
 
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+// const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(porthttp, () => {
     console.log(`Server berjalan pada host ${hostname} dan port ${porthttp}`);
 });
 
-httpsServer.listen(porthttps, () => {
+/* httpsServer.listen(porthttps, () => {
     console.log(`Server berjalan pada host ${hostname} dan port ${porthttps}`);
-});
+}); */
