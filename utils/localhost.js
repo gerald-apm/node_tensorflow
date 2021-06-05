bebd6f8d7f4ab7851db7d38778cce70baf5cb449
Geraldhi A. P. Mahayadnya <geraldiaditya1111@gmail.com>
2021-06-05 19:15:56 +0700

    test change domain

--- utils/localhost.js
+++ utils/localhost.js
@@ -1,4 +1,4 @@
 const hostname = process.env.NODE_ENV !== 'production' ?
-    'localhost' : 'api.easeplantz.ml';
+    'localhost' : process.env.DOMAIN || 'api.easeplantz.ml';
 
 module.exports = hostname;
