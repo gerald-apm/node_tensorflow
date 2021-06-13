af8643a88b7d8a95cac9d252e8808a42b9d613fd
Geraldhi Mahayadnya <geraldiaditya1111@gmail.com>
2021-06-13 21:01:12 +0700

    fixing limit image upload size

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const uploadRouter = require('./routes/upload');
 
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 
 app.use('/', indexRouter);
 app.use('/predict/tomato', tomatoRouter);
