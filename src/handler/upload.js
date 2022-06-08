071235c4f0aa4f178e37c2188ae6528ebf65b8f6
Geraldhi A. P. Mahayadnya <geraldiaditya1111@gmail.com>
2022-06-08 19:15:39 +0800

    fixed delete batch file #4

--- src/handler/upload.js
+++ src/handler/upload.js
@@ -228,13 +228,10 @@ const deleteFileUploadHandler = (req, res) => {
     }
 
     let query = { model };
-    db.predictions.deleteMany(query, (err, result) => {
-      if (err) {
-        res.send(err);
-        console.log(err);
-      } else {
-        // res.json(baseResponse(result));
-      }
+    db.predictions.deleteMany(query, function (err, obj) {
+      if (err) throw err;
+      console.log(obj.result.n + " document(s) deleted");
+      db.close();
     });
 
     console.log("cleared!");
